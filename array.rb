c87f35b4d7c9eeddf7524add3e706cd256a0ee8d
Alysa Barbarisi <baal02@cox.net>
2018-06-05 22:08:37 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,26 @@
+student_names=[]
+# #arrays are formed by using square brakets [  ]
+
+student_names = ["sam","Jess","Alysa","Bob","Harry"]
+# #puts student_names
+
+# #puts [name of your array] prints out the information in your array in the terminal 
+# puts student_names[0]
+
+
+ student_names.push('cardi')
+puts student_names
+
+student_names.push('billy')
+puts student_names
+
+ student_names.push('raylene')
+ puts student_names
+
+student_names.push('matthew','kimberly')
+ puts student_names
+
+#student_names << "mac"
+ puts student_names
+student_names.push('betty','Archie')
+puts student_names.size
